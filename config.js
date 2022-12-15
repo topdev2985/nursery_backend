3f04d236d34d58992676790ce04ed17972002aeb
smartDev212312 <smartdevtop1989@gmail.com>
2022-12-16 07:48:30 +0900

    dburui update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     port:3001,
     // dbURI:'mongodb://127.0.0.1:27017/nursery',
-    dbURI:'mongodb:ac-qbyinoa-shard-00-02.sviemwx.mongodb.net:27017/nursery',
+    dbURI:'mongodb+srv://123:123@cluster0.sviemwx.mongodb.net/?retryWrites=true&w=majority',
     mode:'development'
 }
